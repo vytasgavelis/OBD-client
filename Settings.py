efa5ceb6085ed2fcb87ce8f3d7fb35af85c0edd8
Vytas Gavelis <gavelisvytas@gmail.com>
2020-12-12 11:20:26 +0200

    user settings can be saved

--- Settings.py
+++ Settings.py
@@ -12,6 +12,7 @@ class Settings(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
         self.settings = None
+        self.controller = controller
 
         label = ttk.Label(self, text="Nustatymai", font=LARGEFONT)
         label.grid(row=0, column=4, padx=10, pady=10)
@@ -30,7 +31,25 @@ class Settings(tk.Frame):
             #     can_compare_tests = r['settings']['can_compare_tests']
             # )
 
-            can_compare_tests = tk.StringVar(value=r['settings']['can_compare_tests'])
+            self.can_compare_tests = tk.BooleanVar(value=r['settings']['can_compare_tests'])
             can_compare_tests_check = ttk.Checkbutton(self, text='Naudoti jusu testus palyginimui',
-                                    variable=can_compare_tests,
-                                    onvalue='1', offvalue='0').grid(row = 3, column=1)
+                                    variable=self.can_compare_tests,
+                                    onvalue=True, offvalue=False).grid(row = 3, column=1)
+
+            ttk.Button(self, text="Issaugoti",
+                       command=lambda: self.on_save_button_click()).grid(row=4, column=1)
+
+    def on_save_button_click(self):
+        can_compare_tests = 'false'
+        if self.can_compare_tests.get():
+            can_compare_tests = 'true'
+
+        r = requests.post(
+            'http://localhost:8080/OBD-server/api.php?action=update_user_settings',
+            {
+                'user_id': self.controller.user_id,
+                'can_compare_tests': can_compare_tests
+            }
+        ).json()
+
+        self.controller.show_frame(StartPage.StartPage)
