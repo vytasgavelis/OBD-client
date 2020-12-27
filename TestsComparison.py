db40d3b43790aa342737af976c0768c172a5ced1
Vytas Gavelis <gavelisvytas@gmail.com>
2020-12-27 13:19:08 +0200

    tests comparison layout

--- TestsComparison.py
+++ TestsComparison.py
@@ -10,12 +10,19 @@ class TestsComparison(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
 
-        button2 = ttk.Button(self, text="Grizti",
+        self.configure(bg="#ECECEC")
+
+        self.tests_image = tk.PhotoImage(file='assets/tests.png')
+        label = ttk.Label(self, image=self.tests_image)
+        label.grid(row=1, column=1, columnspan=3, padx=10, pady=10)
+
+        button2 = ttk.Button(self, text="Atgal",
                              command=lambda: controller.show_user_tests(UserTests.UserTests))
-        button2.grid(row=1, column=1, padx=10, pady=10)
+        button2.grid(row=2, column=2, padx=10, pady=10)
 
     def start(self, current_user_test, other_user_test):
         figure = plt.Figure(figsize=(5, 4))
+        figure.patch.set_facecolor('#ECECEC')
         ax = figure.add_subplot(111)
         ax.plot(current_user_test.speed_data, label='Greitis(km/h)')
         ax.plot(current_user_test.maf_data, label='Oro srautas(kg/s)')
@@ -25,10 +32,11 @@ class TestsComparison(tk.Frame):
         ax.legend(title='Parametrai:', loc='upper left', fontsize='x-small')
 
         canvas = FigureCanvasTkAgg(figure, self)
-        canvas.get_tk_widget().grid(row=2, column=1)
-        ttk.Label(self, text='Laikas: ' + format(float(current_user_test.time), '.2f') + 's').grid(row=3, column=1)
+        canvas.get_tk_widget().grid(row=3, column=1)
+        ttk.Label(self, text='Laikas: ' + format(float(current_user_test.time), '.2f') + 's').grid(row=4, column=1)
 
         figure2 = plt.Figure(figsize=(5, 4))
+        figure2.patch.set_facecolor('#ECECEC')
         ax2 = figure2.add_subplot(111)
         ax2.plot(other_user_test.speed_data, label='Greitis(km/h)')
         ax2.plot(other_user_test.maf_data, label='Oro srautas(kg/s)')
@@ -38,7 +46,7 @@ class TestsComparison(tk.Frame):
         ax2.legend(title='Parametrai:', loc='upper left', fontsize='x-small')
 
         canvas2 = FigureCanvasTkAgg(figure2, self)
-        canvas2.get_tk_widget().grid(row=2, column=2)
-        ttk.Label(self, text='Laikas: ' + format(float(other_user_test.time), '.2f') + 's').grid(row=3, column=2)
+        canvas2.get_tk_widget().grid(row=3, column=3)
+        ttk.Label(self, text='Laikas: ' + format(float(other_user_test.time), '.2f') + 's').grid(row=4, column=3)
 
 
